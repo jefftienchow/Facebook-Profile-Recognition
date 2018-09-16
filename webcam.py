56e10900912a2b8f1d9e4bdb3fef72ebd9d90177
Jeff Chow <jeffchow@mit.edu>
2018-09-15 23:27:25 -0400

    added html

--- webcam.py
+++ webcam.py
@@ -1,13 +1,24 @@
-import pygame
-import pygame.camera
-from pygame.locals import *
+# import pygame
+# import pygame.camera
+# from pygame.locals import *
+#
+# pygame.init()
+#
+# pygame.camera.init()
+#
+# camlist = pygame.camera.list_cameras()
+# if camlist:
+#     cam = pygame.camera.Camera(camlist[0],(640,480))
 
-pygame.init()
-
-pygame.camera.init()
-
-camlist = pygame.camera.list_cameras()
-if camlist:
-    cam = pygame.camera.Camera(camlist[0],(640,480))
+from cv2 import *
+# initialize the camera
+cam = VideoCapture(0)   # 0 -> index of camera
+s, img = cam.read()
+if s:    # frame captured without any errors
+    namedWindow("cam-test",CV_WINDOW_AUTOSIZE)
+    imshow("cam-test",img)
+    waitKey(0)
+    destroyWindow("cam-test")
+    imwrite("filename.jpg",img) #save image
 
 
