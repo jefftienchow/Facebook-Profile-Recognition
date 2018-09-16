05cdb8cd451c88a59ae377e083102430acd2ed8e
Jeff Chow <jeffchow@mit.edu>
2018-09-16 01:06:27 -0400

    more html stuffs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+file = None
 
 @app.route('/')
 def home():
@@ -10,6 +12,12 @@ def home():
 def hi():
     return render_template("other.html")
 
+@app.route('/upload', methods = ['POST'])
+def upload():
+    print ("hi")
+    if request.method == 'POST':
+        file = request.files['file']
+        return "yay"
+
 if __name__== "__main__":
     app.run(debug=True)
-    
