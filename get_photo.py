4916dcd65f1771fcbde66cd20ceb08fa829bf897
Dave PH Lin <pohanlin2mit.edu>
2018-09-16 02:38:22 -0400

    ::::)))))

--- get_photo.py
+++ get_photo.py
@@ -8,13 +8,15 @@ def get_photo_func():
 	Save images (profile pics of your friends) to the facebook_pictures folder
 	'''
 	ACCESS_TOKEN = "EAAaJZCDDvNSEBAG4ABvmQFxZARKzel1ZBsRX1KLesHNPFGSC75LiDZCR4dwlWmawWcCAHeO2SoVpH9oCr0tiLnspQ5EUkxeAWzrbN81eQusjqcIw0yWnzO2lx4IQ2JhJP93jZCFH5dZB6GI86rD8chjkOL51cOZBrDLxVuxTHytgKTWdGg8NTmjqxfMZA4ilJAQkFoPfIhde9QZDZD"   
+	ACCESS_TOKEN = 'EAAaJZCDDvNSEBAIF8u2AyRZCDYvenn8QYGmGLTLU0ujyq5NZBCJFXl7iEtmKDHM0An2XXRVXFGZAFUlHEpLfMkC78ONsNsTL5IVvldUx40nb12cC9CL9MGPRNRZCICEY9u8CGmgEmMN2ziGWoEtOw2MdyZAF0TQpwF7WuE2RXzDsqmID9kiaAhwrtxPtCZAougE2QpuPiNZBZBwZDZD'
 	photo_type = 'tagged'
 	r_url = 'https://graph.facebook.com/v3.1/me/friends?access_token=' + ACCESS_TOKEN
 	r = requests.get(r_url)
 	#print(r.json())
 	friend_data = r.json()['data']
-	friend_ids = []
-	propics = []
+	friend_ids = [] # a list of ids
+	friend_names = []
+	propics = [] # a list of f_urls
 
 	directory = 'facebook_pictures'
 	if not os.path.exists(directory):
@@ -22,21 +24,18 @@ def get_photo_func():
 
 	print('locating tagged photos on facebook...')
 
-        
-	for friend in friend_data:
-		friend_ids.append(friend['id'])
+	friend_ids = [friend['id'] for friend in friend_data]
+	friend_names = [friend['name'] for friend in friend_data]
 
 	for id in friend_ids:
 		f_url = 'https://graph.facebook.com/v3.1/' + id + '/picture?height=500&width=500' + '&access_token=' + ACCESS_TOKEN
-		print(f_url)
+		#print(f_url)
 		propics.append(f_url)
-		id_photo = f_url.split('/')[4]
-
 
 	print('downloading photos...')
 
 	for i,propic in enumerate(propics):
-		print(propic)
+		#print(propic)
 		urllib.request.urlretrieve(propic, os.path.join(directory, friend_ids[i] + '.jpg'))
 
 
--- pipeline.py
+++ pipeline.py
@@ -1,10 +1,14 @@
 from cv2 import *
 import numpy as np
 import os
+import facebook
 
 import get_photo
 import webcam
 import facial_recog
+
+ACCESS_TOKEN = 'EAAaJZCDDvNSEBAIF8u2AyRZCDYvenn8QYGmGLTLU0ujyq5NZBCJFXl7iEtmKDHM0An2XXRVXFGZAFUlHEpLfMkC78ONsNsTL5IVvldUx40nb12cC9CL9MGPRNRZCICEY9u8CGmgEmMN2ziGWoEtOw2MdyZAF0TQpwF7WuE2RXzDsqmID9kiaAhwrtxPtCZAougE2QpuPiNZBZBwZDZD'
+	
 
 # Takes a picture and then returns the facebook profile
 
@@ -12,21 +16,26 @@ def main():
 	get_photo.get_photo_func()
 	webcam.capture()
 	paths = get_picture_paths()
+	ordered_ids = [path.split('/')[1] for path in paths]
 	results = facial_recog.face_recog(paths, 'filename.jpg')
 	print(results)
+	target = [ordered_ids[i].split('.')[0] for i in range(len(paths)) if results[i]][0]
 
+	graph = facebook.GraphAPI(access_token= ACCESS_TOKEN, version="3.0")
+	target_info = graph.get_object(id=target)
+	print(target_info)
 
 def get_picture_paths():
 	return ['facebook_pictures/'+filename for filename in os.listdir('facebook_pictures')]
 
 
 
-def link_to_profile_url(file):
-        url_dic = {}
-        f = open(file,'r')
-        for i in f.readlines():
-                id, id_photo = i.split('\t')
-                url_dic[id_photo] = id 
+##def link_to_profile_url(file):
+##        url_dic = {}
+##        f = open(file,'r')
+##        for i in f.readlines():
+##                id, id_photo = i.split('\t')
+##                url_dic[id_photo] = id 
         
 
 
